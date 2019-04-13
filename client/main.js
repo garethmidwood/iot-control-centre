7f46e96346f7abebff779b5288ca8189a90c303a
Gareth Midwood <gareth.midwood@creode.co.uk>
2019-04-14 00:51:05 +0100

    Add bpm to dashboard and implement beat mechanism

--- client/main.js
+++ client/main.js
@@ -3,56 +3,54 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Meteor.subscribe('options');
+Meteor.subscribe('devices');
+Meteor.subscribe('config');
+
+var bpm = 60;
+
+
+
+
 
 Template.body.helpers({
-    totalVotes: function() {
-        var total = 0;
-        var options = OptionsCollection.find({});
+    devices: function() {
+        return DevicesCollection.find({}, {sort: {deviceType: 1, deviceName: 1}});
+    }
+});
 
-        options.forEach(function(item) {
-            total += (item.votes ? item.votes : 0);
-        });
-
-        return total;
-    },
-    options: function() {
-        return OptionsCollection.find({});
+Template.beat_counter.helpers({
+    config: function() {
+        return ConfigCollection.find({_id: 'bpm'});
     }
 });
 
 
 
-
-
+/*
 Template.body.events({
     'click button.reset'(event, instance) {
-        var options = OptionsCollection.find({});
+        var options = DevicesCollection.find({});
 
         options.forEach(function(item) {
-            OptionsCollection.update(item._id, {$set: {votes: 0, winner: false}});
+            DevicesCollection.update(item._id, {$set: {votes: 0, winner: false}});
         });
     }
 });
+*/
 
 
 
 
 
-Template.option.helpers({
-    votepercentage: function() {
-        return  100 * (this.votes / 10);
-    }
-});
-
-
-Template.option.events({
+/*
+Template.device.events({
     'click button'(event, instance) {
         // increment the votes counter when button is clicked
         newVotes = 1 + (this.votes ? this.votes : 0);
 
         var isWinner = (newVotes >= 10);
 
-        OptionsCollection.update(this._id, {$set: {votes: newVotes, winner: isWinner}});
+        DevicesCollection.update(this._id, {$set: {votes: newVotes, winner: isWinner}});
     }
 });
+*/
--- server/main.js
+++ server/main.js
@@ -1,10 +1,97 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.publish("options", function() {
-    return OptionsCollection.find({});
+var beatTimout;
+
+Meteor.publish("config", function() { return ConfigCollection.find({}); });
+
+Meteor.publish("devices", function() { return DevicesCollection.find({}); });
+
+
+Meteor.publish("led1", function() { return Led1Collection.find({}); });
+Meteor.publish("led2", function() { return Led2Collection.find({}); });
+Meteor.publish("led3", function() { return Led3Collection.find({}); });
+Meteor.publish("led4", function() { return Led4Collection.find({}); });
+Meteor.publish("led5", function() { return Led5Collection.find({}); });
+Meteor.publish("led6", function() { return Led6Collection.find({}); });
+Meteor.publish("led7", function() { return Led7Collection.find({}); });
+Meteor.publish("led8", function() { return Led8Collection.find({}); });
+
+
+Meteor.publish("hue1", function() { return Hue1Collection.find({}); });
+Meteor.publish("hue2", function() { return Hue2Collection.find({}); });
+Meteor.publish("hue3", function() { return Hue3Collection.find({}); });
+Meteor.publish("hue4", function() { return Hue4Collection.find({}); });
+Meteor.publish("hue5", function() { return Hue5Collection.find({}); });
+Meteor.publish("hue6", function() { return Hue6Collection.find({}); });
+
+
+Meteor.publish("sound", function() { return SoundCollection.find({}); });
+
+
+Meteor.onConnection(function (connection) {
+   console.log("New DDP Connection:", connection.id);
+
+   connection.onClose(function() {
+      console.log("DDP Disconnect:", connection.id); 
+      // console.log(DevicesCollection.findOne( { _id: connection.id } ));
+      DevicesCollection.remove( { _id: connection.id } );
+   });
 });
 
-//Meteor.startup(() => {
-//  // code to run on server at startup
-//    console.log('starting..');
-//});
+
+Meteor.methods({
+   'device_online': function(deviceName, deviceType) {
+        DevicesCollection.insert({_id: this.connection.id, deviceName: deviceName, deviceType: deviceType});
+
+        console.log('a ' + deviceType + ' is online, called ' + deviceName);
+        return 'device is registered online';
+   },
+   'set_beat': function(beat) {
+        console.log('submitted beat of ' + beat);
+        ConfigCollection.update({_id: 'bpm'}, {value: beat});
+
+        var pulseRate = 60000 / beat;
+
+        setNewBeat(pulseRate);
+        return 'beat is now ' + beat;
+   }
+});
+
+
+Meteor.startup(() => {
+    // code to run on server at startup
+    console.log('Clearing all devices');
+    DevicesCollection.remove({});
+
+    console.log('Resetting config');
+    ConfigCollection.remove({});
+    ConfigCollection.insert({_id: 'bpm', value: 60});
+
+    // default beat - 1second
+    beat(1000);
+});
+
+
+
+function setNewBeat(pulseRate) {
+  console.log('Setting new pulse rate to ' + pulseRate);
+  Meteor.clearTimeout(beatTimout);
+  beat(pulseRate);
+}
+
+function beat(pulseRate) {
+    beatTimout = Meteor.setTimeout(function () {
+        console.log('beat at ' + pulseRate);
+
+        onBeat();
+
+        beat(pulseRate);
+    }, pulseRate);
+}
+
+function onBeat() {
+  // clear collections
+  Led1Collection.remove({});
+
+  Led1Collection.insert({colour: "red"});
+}
