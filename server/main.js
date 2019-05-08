91dccbb788ee0cc68f4b1b98954de7e146643ea5
Gareth Midwood <gareth.midwood@creode.co.uk>
2019-05-08 11:51:07 +0100

    Update instrument

--- server/main.js
+++ server/main.js
@@ -275,5 +275,5 @@ function addNoteToSequence(note){
 
 function playNote(note) {
   SoundCollection.remove({});
-  SoundCollection.insert({notes: note,instrument:'piano'});
+  SoundCollection.insert({notes: note,instrument:'bells'});
 }
